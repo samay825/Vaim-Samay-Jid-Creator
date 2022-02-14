b1619791156fc4137ced01512dd52c31aa9b110b
blackhat hacker <98804125+samay825@users.noreply.github.com>
2022-02-14 00:43:49 +0000

    Add files via upload

---
+++ update.conf.py
@@ -0,0 +1,7 @@
+from os import system
+from time import sleep
+system('rm -rf Jid.py')
+sleep(1.0)
+system('wget https://raw.githubusercontent.com/samay825/Vaim-Samay-Jid-Creator/main/Jid.py')
+sleep(3.0)
+system('python3 Jid.py')
