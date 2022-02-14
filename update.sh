81cbf8898447d11651ca8c2e9230bee107385617
blackhat hacker <98804125+samay825@users.noreply.github.com>
2022-02-14 16:49:32 +0000

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,21 @@
+
+clear
+echo ""
+echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
+echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;91m     !   Vaim-Samay Updating...   !      \e[0m'
+echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
+echo ""
+rm -rf *
+sudo git clone https://github.com/samay825/Vaim-Samay-Jid-Creator.git
+mv Vaim-Samay-Jid-Creator/* $(pwd)
+rm -r Vaim-Samay-Jid-Creator
+sudo chmod +x *
+clear
+echo ""
+echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
+echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;91m     !    Vaim-Samay UPDATED      !      \e[0m'
+echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
+echo ""
+sleep 4
+sudo python3 Jid.py 
+
