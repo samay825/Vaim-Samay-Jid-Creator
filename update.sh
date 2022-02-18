f5cead214672667f6fc139dbb253c90a58e9a887
blackhat hacker <98804125+samay825@users.noreply.github.com>
2022-02-18 07:40:16 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,5 +17,5 @@ echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;91m     !    Vaim-Samay U
 echo -e $'\e[1;33m\e[0m\e[1;77m \e[0m\e[1;33m\e[0m\e[1;36m       ---------------------------        \e[0m'
 echo ""
 sleep 4
-sudo python3 Jid.py 
+python3 Jid.py 
 
